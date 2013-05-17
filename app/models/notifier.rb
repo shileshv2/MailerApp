be7a1d57aafcd8d86bff148269a5adab1a28dd37
ganeshan-v2 <ganeshan.a@in.v2solutions.com>
2013-05-17 19:34:31 +0530

    added missing file

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -8,13 +8,21 @@ class Notifier < ActionMailer::Base
     content_type "multipart/mixed"
 
     part "text/html" do |p|
-           p.body = render_message("text_html", :email_body => email_body)
+           p.body = render_message("text_html", :email_body => get_email_body(email_body), :image => uploaded_image, :user => user)
     end
     
-    attachment :content_type => 'application/image',
-             :body => File.read("#{RAILS_ROOT}/public/images/#{uploaded_image}"),
-             :filename => uploaded_image
+    inline_attachment :content_type => "image/jpeg",
+    :body => File.read("#{RAILS_ROOT}/public/images/#{uploaded_image}"),
+    :filename => "#{uploaded_image}",
+    :cid => "#{uploaded_image}"
+
   end
 
 
+  def get_email_body(body)
+    body_content= body.split("</h1>")
+    body_before_alt_in_img=body_content.shift
+    body_after_alt_in_img= body_content.shift
+    return (body_after_alt_in_img)
+  end
 end
